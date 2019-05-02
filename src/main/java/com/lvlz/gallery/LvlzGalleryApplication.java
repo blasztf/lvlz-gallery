c3f4aaebb1bc23d09b1bd0a0052e332795fa4382
doodlyz <ywhy1103@gmail.com>
2019-05-03 05:29:15 +0700

    First commit

---
+++ src/main/java/com/lvlz/gallery/LvlzGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.lvlz.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvlzGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvlzGalleryApplication.class, args);
+	}
+
+}
